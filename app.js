9daea56a81e39c6e8dbb4d42f6426525bb033163
jhyan <victoryan1590@gmail.com>
2021-04-29 21:13:52 -0700

    tryout to add a coment

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 // app.js
 App({
-  onShow: function () {
-    console.log("life cycle: app onShow")
-  },
-  onHide: function () {
-    console.log("life cycle: app onHide")
-  },
   onLaunch: function (opt) {
-    console.log("life cycle: app onLaunch")
+    console.log('life cycle: app onlaunch')
     console.log(opt)
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    // 登录!
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
